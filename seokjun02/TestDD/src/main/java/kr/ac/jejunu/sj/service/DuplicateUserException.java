27af0b0355a86c637f42e655fc1ea66c43dba561
seokjun02 <seokjun02@gmail.com>
2012-04-19 18:05:11 +0000

    commit files

---
+++ seokjun02/TestDD/src/main/java/kr/ac/jejunu/sj/service/DuplicateUserException.java
@@ -0,0 +1,6 @@
+package kr.ac.jejunu.sj.service;
+
+public class DuplicateUserException extends RuntimeException {
+	private static final long serialVersionUID = -7769587502140805428L;
+
+}
--- seokjun02/TestDD/src/main/java/kr/ac/jejunu/sj/service/UserService.java
+++ seokjun02/TestDD/src/main/java/kr/ac/jejunu/sj/service/UserService.java
@@ -1,5 +1,20 @@
 package kr.ac.jejunu.sj.service;
 
+import java.util.List;
+
+import kr.ac.jejunu.sj.model.User;
+import kr.ac.jejunu.sj.repository.UserRepository;
+
 public interface UserService {
+	public User get(String id);
+
+	public void removeById(String id);
+
+	public void addUser(User user);
+
+	public void modifyUser(User user);
+
+	public List<User> listUser();
 	
+	public void setRepository(UserRepository repository);
 }
