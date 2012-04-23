bd08e39b5d5d2dc35032abed37e6430d250451d1
yjw2288@gmail.com <yjw2288@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-23 12:44:51 +0000

    CreateUser 로직 작성, 테스트 통과

--- trunk/yjw2288/usermanager/src/main/java/kr/ac/jejuuniv/controller/CreateUser.java
+++ trunk/yjw2288/usermanager/src/main/java/kr/ac/jejuuniv/controller/CreateUser.java
@@ -1,7 +1,10 @@
 package kr.ac.jejuuniv.controller;
 
+import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/save")
 public class CreateUser {
+	@Autowired
+	private UserService service;
+
 	public String action(@RequestParam("id") String id,
 			@RequestParam("name") String name,
 			@RequestParam("password") String password, ModelMap model) {
+		User user = new User(id, name, password);
+		service.add(user);
+
 		return "list";
 	}
 }
