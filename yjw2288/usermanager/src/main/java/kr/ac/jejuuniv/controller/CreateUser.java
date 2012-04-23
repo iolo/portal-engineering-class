c401f609a781a71dde6aff4a9b107eac946c75aa
yjw2288 <yjw2288@gmail.com>
2012-04-23 12:43:22 +0000

    CreateUserController 정식 테스트, 성공 못함, UserRepository 에서 해당 User값을 찾지 못한다는 Exception 발생

--- yjw2288/usermanager/src/main/java/kr/ac/jejuuniv/controller/CreateUser.java
+++ yjw2288/usermanager/src/main/java/kr/ac/jejuuniv/controller/CreateUser.java
@@ -1,12 +1,17 @@
 package kr.ac.jejuuniv.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/save")
 public class CreateUser {
-	public String action() {
+	public String action(@RequestParam("id") String id,
+			@RequestParam("name") String name,
+			@RequestParam("password") String password, ModelMap model) {
 		return "list";
 	}
 }
--- yjw2288/usermanager/src/test/java/CreateUserControllerTest.java
+++ yjw2288/usermanager/src/test/java/CreateUserControllerTest.java
@@ -1,20 +1,38 @@
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import kr.ac.jejuuniv.controller.CreateUser;
+import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ui.ModelMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-servlet-context.xml")
 public class CreateUserControllerTest {
 	@Autowired
 	private CreateUser controller;
+	@Autowired
+	private UserService service;
 
 	@Test
 	public void testController() {
-		assertEquals("list", controller.action());
+		String id = "1";
+		String name = "양진원";
+		String password = "abcd";
+
+		ModelMap mm = new ModelMap();
+
+		assertThat("list", is(controller.action(id, name, password, mm)));
+
+		User user = service.getUser(id);
+
+		assertThat(id, is(user.getId()));
+		assertThat(name, is(user.getName()));
+		assertThat(password, is(user.getPassword()));
 	}
 }
