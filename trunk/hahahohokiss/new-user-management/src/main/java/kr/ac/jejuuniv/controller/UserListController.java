a9d7deba7708809022fed15af0cfbbd8063db844
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-25 14:00:01 +0000

    - UserListControllerTest class 추가/testList method 추가 - UserListController class 추

---
+++ trunk/hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserListController.java
@@ -0,0 +1,14 @@
+package kr.ac.jejuuniv.controller;
+
+import java.util.List;
+
+import kr.ac.jejuuniv.model.User;
+
+public class UserListController {
+
+	
+	public List<User> list() {
+		return null;
+	}
+
+}
