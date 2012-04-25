4f0a6568d45340d664fe761a86a3506040262511
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-25 07:56:29 +0000

    - UserServiceTest refactoring  version 1 (@Before추가 및 코드 중복 없앰)

--- hahahohokiss/new-user-management/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
+++ hahahohokiss/new-user-management/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
@@ -12,6 +12,7 @@ import kr.ac.jejuuniv.exception.DataNotFoundException;
 import kr.ac.jejuuniv.model.User;
 import kr.ac.jejuuniv.repository.UserRepository;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -22,32 +23,24 @@ import org.springframework.dao.DuplicateKeyException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
-	/*
-	 * User List 가져오기(실패)
-	 * User id 로 가져오기(실패)
-	 * User 추가하기 (실패)
-	 * User 수정하기 (실패)
-	 * User 삭제하기 (실패)
-	 */
 
 	@Mock
 	UserRepository userRepository;
 	
 	UserService userService; 
 	
+	@Before
+	public void setup() {
+		userService = new UserServiceImpl(userRepository);
+	}
 	
 	@Test
 	public void testUserList() {
-		// UserList를 가져오고 list 에 값이 있는지, 첫번째 id 가 0인지 확인
 		List<User> users = new ArrayList<User>();
-		userService = new UserServiceImpl(userRepository);
 		when(userRepository.findAll()).thenAnswer(new Answer<List<User>>() {
 			public List<User> answer(InvocationOnMock invocation) throws Throwable {
 				List<User> users = new ArrayList<User>();
-				User user = new User();
-				user.setId("0");
-				user.setName("한진수");
-				user.setPassword("비밀번호");
+				User user = setUserInfo();
 				users.add(user);
 				return users;
 			}
@@ -59,13 +52,9 @@ public class UserServiceTest {
 	
 	@Test
 	public void testUserGetSuccess() {
-		userService = new UserServiceImpl(userRepository);
 		when(userRepository.findById("0")).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
-				User user = new User();
-				user.setId("0");
-				user.setName("한진수");
-				user.setPassword("비밀번호");
+				User user = setUserInfo();
 				return user;
 			}
 		});
@@ -75,27 +64,23 @@ public class UserServiceTest {
 		assertThat(user.getName(), is("한진수"));
 		assertThat(user.getPassword(), is("비밀번호"));
 	}
+
 	
 	@Test(expected=DataNotFoundException.class)
 	public void testUserGetFail() {
-		userService = new UserServiceImpl(userRepository);
 		when(userRepository.findById("0")).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				return null;
 			}
 		});
-		User user = userService.get("0");
+		userService.get("0");
 		
 	}
 	
 	
 	@Test
 	public void testUserAddSuccess() {
-		userService = new UserServiceImpl(userRepository);
-		User user = new User();
-		user.setId("0");
-		user.setName("한진수");
-		user.setPassword("비밀번호");
+		User user = setUserInfo();
 		when(userRepository.insert(user)).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				return (User)invocation.getArguments()[0];
@@ -112,39 +97,26 @@ public class UserServiceTest {
 	
 	@Test(expected=DuplicateKeyException.class)
 	public void testUserAddFail() {
-		userService = new UserServiceImpl(userRepository);
-		User user = new User();
-		user.setId("0");
-		user.setName("한진수");
-		user.setPassword("비밀번호");
+		User user = setUserInfo();
 		
 		when(userRepository.findById(user.getId())).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
-				User user = new User();
-				user.setId("0");
-				user.setName("한진수");
-				user.setPassword("비밀번호");
+				User user = setUserInfo();
 				return user;
 			}
 		});
 		
 		userService.add(user);
 	}
+
 	
 	@Test()
 	public void testUserModifySuccess() {
-		userService = new UserServiceImpl(userRepository);
-		User user = new User();
-		user.setId("0");
-		user.setName("한진수");
-		user.setPassword("비밀번호");
+		User user = setUserInfo();
 		
 		when(userRepository.findById("0")).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
-				User user = new User();
-				user.setId("0");
-				user.setName("한진수");
-				user.setPassword("비밀번호");
+				User user = setUserInfo();
 				return user;
 			}
 		});
@@ -164,11 +136,7 @@ public class UserServiceTest {
 	
 	@Test(expected=DataNotFoundException.class)
 	public void testUserModifyFail() {
-		userService = new UserServiceImpl(userRepository);
-		User user = new User();
-		user.setId("0");
-		user.setName("한진수");
-		user.setPassword("비밀번호");
+		User user = setUserInfo();
 		
 		when(userRepository.findById(user.getId())).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
@@ -181,13 +149,9 @@ public class UserServiceTest {
 	
 	@Test
 	public void testUserDeleteSuccess() {
-		userService = new UserServiceImpl(userRepository);
-		doAnswer(new Answer() {
+		doAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
-				User user = new User();
-				user.setId("0");
-				user.setName("한진수");
-				user.setPassword("password");
+				User user = setUserInfo();
 				return user;
 			}
 		}).when(userRepository).findById("0");
@@ -196,12 +160,19 @@ public class UserServiceTest {
 	
 	@Test(expected=DataNotFoundException.class)
 	public void testUserDeleteFail() {
-		userService = new UserServiceImpl(userRepository);
-		doAnswer(new Answer() {
+		doAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				return null;
 			}
 		}).when(userRepository).findById("0");
 		userService.delete("0");
+	}
+
+	private User setUserInfo() {
+		User user = new User();
+		user.setId("0");
+		user.setName("한진수");
+		user.setPassword("비밀번호");
+		return user;
 	}
 }
