d295969aa03737a0f8aa7f2c2d507411be8b7ff6
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-25 14:21:33 +0000

    - UserListController class의 testList method 성공

--- hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserListController.java
+++ hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserListController.java
@@ -3,12 +3,18 @@ package kr.ac.jejuuniv.controller;
 import java.util.List;
 
 import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
 
 public class UserListController {
 
+	UserService userService;
+	
+	public UserListController(UserService userService) {
+		this.userService = userService;
+	}
 	
 	public List<User> list() {
-		return null;
+		return userService.list();
 	}
 
 }
