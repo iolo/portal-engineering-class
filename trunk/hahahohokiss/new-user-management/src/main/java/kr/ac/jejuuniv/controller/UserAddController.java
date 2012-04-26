18baef4e3429793faaa057f6f41d1933c55ff1fd
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-26 12:01:44 +0000

    - UserAddControllerTest 성공!

--- trunk/hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserAddController.java
+++ trunk/hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserAddController.java
@@ -3,14 +3,20 @@ package kr.ac.jejuuniv.controller;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
 
 public class UserAddController {
 
-	public ModelAndView add(User user) {
-		return null;
-	}
+	UserService userService;
 	
-	/**
-	 * @param args
-	 */
+	public UserAddController(UserService userService) {
+		this.userService = userService;
+	}
+
+	public ModelAndView add(User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject(userService.add(user));
+		modelAndView.setViewName("/list");
+		return modelAndView;
+	}
 }
--- trunk/hahahohokiss/new-user-management/src/test/java/kr/ac/jejuuniv/controller/UserAddControllerTest.java
+++ trunk/hahahohokiss/new-user-management/src/test/java/kr/ac/jejuuniv/controller/UserAddControllerTest.java
@@ -1,36 +1,48 @@
 package kr.ac.jejuuniv.controller;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.web.servlet.ModelAndView;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserAddControllerTest {
 	UserAddController userAddController;
-	MockHttpServletRequest request;
-	MockHttpServletResponse response;
 
+	
+	@Mock
+	UserService userService;
+	
 	@Test
 	public void testAdd() {
 		// User 값 입력
 		// redirect:list
-		
-		userAddController = new UserAddController();
-		
+		userAddController = new UserAddController(userService);
 		User user = new User();
 		user.setId("0");
 		user.setName("한진수");
 		user.setPassword("비밀번호");
-		ModelAndView modelAndView = userAddController.add(user);
 		
-		assertThat(((User)modelAndView.getModel().get("user")).getId(), is("0"));
-		assertThat(((User)modelAndView.getModel().get("user")).getName(), is("한진수"));
-		assertThat(((User)modelAndView.getModel().get("user")).getPassword(), is("비밀번호"));
-		assertThat(modelAndView.getViewName(), is("redirect/:list"));
+		ModelAndView modelAndView = userAddController.add(user);
+		when(userService.add(user)).thenAnswer(new Answer<User>() {
+			public User answer(InvocationOnMock invocation) throws Throwable {
+				return (User)invocation.getArguments()[0];
+			}
+		});
+		user = (User)modelAndView.getModel().get("user");
+		assertThat(user.getId(), is("0"));
+		assertThat(user.getName(), is("한진수"));
+		assertThat(user.getPassword(), is("비밀번호"));
+		assertThat(modelAndView.getViewName(), is("/list"));
 		
 	}
 }
