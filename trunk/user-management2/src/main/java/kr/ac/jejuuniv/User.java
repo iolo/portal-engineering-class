bac40cb27536a308d3ef1d59065561e0a37278c3
sdxd0919@gmail.com <sdxd0919@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-29 15:47:53 +0000

    list & delete 구현

---
+++ trunk/user-management2/src/main/java/kr/ac/jejuuniv/User.java
@@ -0,0 +1,41 @@
+package kr.ac.jejuuniv;
+
+public class User {
+
+	private String id;
+	private String name;
+	private Object password;
+
+	public User(String id, String name, String password) {
+		this.setId(id);
+		this.setName(name);
+		this.setPassword(password);
+	}
+
+	public User() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getPassword() {
+		return password;
+	}
+
+	public void setPassword(Object password) {
+		this.password = password;
+	}
+}
---
+++ trunk/user-management2/src/main/java/kr/ac/jejuuniv/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package kr.ac.jejuuniv;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+
+	public void remove(String id) {
+		userRepository.deleteById(id);
+	}
+
+}
