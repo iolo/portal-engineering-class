561ee648370ce7dd85d8908d91d8a8a60cb6df55
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-04-29 05:21:46 +0000

    - dbunit 제거 및 UserAddControllerTest 실패 테스트 추

--- hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserListController.java
+++ hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserListController.java
@@ -2,17 +2,28 @@ package kr.ac.jejuuniv.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import kr.ac.jejuuniv.model.User;
 import kr.ac.jejuuniv.service.UserService;
 
+@Controller
+@RequestMapping("/list")
 public class UserListController {
-
+	
 	UserService userService;
 	
+	public UserListController() {
+		
+	}
+	
+	@Autowired
 	public UserListController(UserService userService) {
 		this.userService = userService;
 	}
-	
+	@RequestMapping
 	public List<User> list() {
 		return userService.list();
 	}
--- hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserModifyController.java
+++ hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/UserModifyController.java
@@ -1,15 +1,23 @@
 package kr.ac.jejuuniv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import kr.ac.jejuuniv.model.User;
 import kr.ac.jejuuniv.service.UserService;
 
+@Controller
+@RequestMapping("/modify")
 public class UserModifyController {
 	UserService userService;
 	
+	@Autowired
 	public UserModifyController(UserService userService) {
 		this.userService = userService;
 	}
 	
+	@RequestMapping
 	public User modify(String id) {
 		return userService.get(id);
 	}
