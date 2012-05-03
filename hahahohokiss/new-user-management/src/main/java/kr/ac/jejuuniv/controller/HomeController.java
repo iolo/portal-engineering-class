4d6dc4874ae5d87324066e84adbba2006138cfb3
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-05-03 15:35:32 +0000

    - HomeControllerTest 생성 및 testAction method 작성(HomeController method 추출)

---
+++ hahahohokiss/new-user-management/src/main/java/kr/ac/jejuuniv/controller/HomeController.java
@@ -0,0 +1,16 @@
+package kr.ac.jejuuniv.controller;
+
+import kr.ac.jejuuniv.service.UserService;
+
+public class HomeController {
+	
+	public HomeController() {
+		
+	}
+	public HomeController(UserService userService) {
+		
+	}
+	public String action() {
+		return null;
+	}
+}
