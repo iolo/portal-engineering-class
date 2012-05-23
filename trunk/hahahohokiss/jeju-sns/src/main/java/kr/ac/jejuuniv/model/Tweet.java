e6569133942bd9733d6f858abacf9954ae8b79d9
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-05-23 17:28:37 +0000

    - PersonalPageTest 추가 및 컴파일 오류 제거

---
+++ trunk/hahahohokiss/jeju-sns/src/main/java/kr/ac/jejuuniv/model/Tweet.java
@@ -0,0 +1,41 @@
+package kr.ac.jejuuniv.model;
+
+public class Tweet {
+	private String userId;
+	private String imageUrl;
+	private String contentId;
+	private String content;
+	private String date;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public String getContentId() {
+		return contentId;
+	}
+	public void setContentId(String contentId) {
+		this.contentId = contentId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
---
+++ trunk/hahahohokiss/jeju-sns/src/main/java/kr/ac/jejuuniv/service/PersonalPage.java
@@ -0,0 +1,17 @@
+package kr.ac.jejuuniv.service;
+
+import java.util.List;
+
+import kr.ac.jejuuniv.model.Tweet;
+
+public class PersonalPage {
+
+	public PersonalPage(TweetRepository tweetRepository) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Tweet> getList(String userId) {
+		return null;
+	}
+
+}
