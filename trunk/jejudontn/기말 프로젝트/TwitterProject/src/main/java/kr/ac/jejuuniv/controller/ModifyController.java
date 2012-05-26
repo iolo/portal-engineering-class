3bd5f10e0e0001f798d85ce89a76ffaf891b29e7
jejudontn <jejudontn@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-05-26 07:11:32 +0000

    사용자 수정 완료

--- trunk/jejudontn/기말 프로젝트/TwitterProject/src/main/java/kr/ac/jejuuniv/controller/ModifyController.java
+++ trunk/jejudontn/기말 프로젝트/TwitterProject/src/main/java/kr/ac/jejuuniv/controller/ModifyController.java
@@ -1,16 +1,41 @@
 package kr.ac.jejuuniv.controller;
 
+import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/modify")
+@SessionAttributes("user")
 public class ModifyController {
 	
+	@Autowired
+	UserService userService;
+
 	@RequestMapping()
-	public void modify(){
+	public User modify(@ModelAttribute User user) {
+		return user;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String userModify(@ModelAttribute User user,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("information") String information,
+			@RequestParam("profileImage") MultipartFile profileImage) {
+		String id = user.getId();
+		userService.modifyUser(id,password, name, information, profileImage);
 		
-				
+		
+		return "redirect:/main";
 	}
 
 }
