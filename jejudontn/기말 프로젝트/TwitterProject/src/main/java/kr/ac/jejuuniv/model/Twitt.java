4be9c728c138ef32e7ea9714d742df47aad92c0e
jejudontn <jejudontn@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-05-26 06:19:32 +0000

    사용자 등록, 트윗 쓰기 완료

---
+++ jejudontn/기말 프로젝트/TwitterProject/src/main/java/kr/ac/jejuuniv/model/Twitt.java
@@ -0,0 +1,53 @@
+package kr.ac.jejuuniv.model;
+
+public class Twitt {
+
+	private String twiitID;
+	private String id;
+	private String writrDate;
+	private String name;
+	private String profilImageURI;
+	private String twiitt;
+	
+	
+	public String getTwiitID() {
+		return twiitID;
+	}
+	public void setTwiitID(String twiitID) {
+		this.twiitID = twiitID;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getWritrDate() {
+		return writrDate;
+	}
+	public void setWritrDate(String writrDate) {
+		this.writrDate = writrDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfilImageURI() {
+		return profilImageURI;
+	}
+	public void setProfilImageURI(String profilImageURI) {
+		this.profilImageURI = profilImageURI;
+	}
+	
+	public String getTwiitt() {
+		return twiitt;
+	}
+	public void setTwiitt(String twiitt) {
+		this.twiitt = twiitt;
+	}
+	
+
+	
+}
