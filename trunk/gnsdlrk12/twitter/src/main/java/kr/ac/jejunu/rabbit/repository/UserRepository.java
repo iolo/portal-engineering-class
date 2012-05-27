8ec775007a7ef4f21ff5d8ba8a9e5c8b4b0fdcc1
gnsdlrk12@gmail.com <gnsdlrk12@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-05-27 05:36:49 +0000

    update test 성공

--- trunk/gnsdlrk12/twitter/src/main/java/kr/ac/jejunu/rabbit/repository/UserRepository.java
+++ trunk/gnsdlrk12/twitter/src/main/java/kr/ac/jejunu/rabbit/repository/UserRepository.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository {
+	
 	void insert(User user);
+	
+	void update(User user);
 
 	User findById(String userid);
+	
 }
