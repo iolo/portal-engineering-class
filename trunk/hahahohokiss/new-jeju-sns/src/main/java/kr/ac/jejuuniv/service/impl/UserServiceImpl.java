7dcea5cc2446a24a780631a43e8801b98f58d526
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-01 05:16:06 +0000

    - Service 수정 및 Test 추가 및 성공

--- trunk/hahahohokiss/new-jeju-sns/src/main/java/kr/ac/jejuuniv/service/impl/UserServiceImpl.java
+++ trunk/hahahohokiss/new-jeju-sns/src/main/java/kr/ac/jejuuniv/service/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package kr.ac.jejuuniv.service.impl;
 
+import java.util.List;
+
 import kr.ac.jejuuniv.exception.UserNotFoundException;
+import kr.ac.jejuuniv.model.Follow;
 import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.repository.FollowRepository;
 import kr.ac.jejuuniv.repository.UserRepository;
 import kr.ac.jejuuniv.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
 	UserRepository userRepository;
+	FollowRepository followRepository;
 	
 	public UserServiceImpl() {
 		
@@ -15,6 +20,14 @@ public class UserServiceImpl implements UserService {
 	
 	public UserServiceImpl(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+	
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public void setFollowRepository(FollowRepository followRepository) {
+		this.followRepository = followRepository;
 	}
 	
 	public User getUserByUserId(String id) {
@@ -32,4 +45,17 @@ public class UserServiceImpl implements UserService {
 		return	userRepository.insert(user);
 	}
 
+	public String registerFollowing(String followerId, String followingId) {
+		Follow follow = followRepository.addFollowing(followerId, followingId);
+		return follow.getFollowingUserId();
+	}
+
+	public List<User> getFollower(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getFollowersId(String id) {
+		return followRepository.getFollowerId(id);
+	}
 }
--- trunk/hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
+++ trunk/hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
@@ -3,8 +3,14 @@ package kr.ac.jejuuniv.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import kr.ac.jejuuniv.exception.UserNotFoundException;
+import kr.ac.jejuuniv.model.Follow;
 import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.repository.FollowRepository;
 import kr.ac.jejuuniv.repository.UserRepository;
 import kr.ac.jejuuniv.service.impl.UserServiceImpl;
 
@@ -18,12 +24,18 @@ import org.mockito.stubbing.Answer;
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 	@Mock
-	UserRepository userRepositoy;
+	UserRepository userRepository;
 	
+	@Mock
+	FollowRepository followRepository;
+	
+	// User ID 로 User 가져오기
 	@Test
 	public void testGetUserByUserIdSuccess() {
-		UserService userService = new UserServiceImpl(userRepositoy);
-		when(userRepositoy.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
+		UserService userService = new UserServiceImpl();
+		userService.setUserRepository(userRepository);
+		
+		when(userRepository.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				User user = new User();
 				user.setId("hahahohokiss");
@@ -34,10 +46,25 @@ public class UserServiceTest {
 		assertThat(user.getId(), is("hahahohokiss"));
 	}
 	
+	@Test(expected = UserNotFoundException.class)
+	public void testGetUserByUserIdFail() {
+		UserService userService = new UserServiceImpl();
+		userService.setUserRepository(userRepository);
+		when(userRepository.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
+			public User answer(InvocationOnMock invocation) throws Throwable {
+				return null;
+			}
+		});
+		userService.getUserByUserId("hahahohokiss");
+	}
+
+	// User Id 로 Password 가져오기 (로그인)
 	@Test
 	public void testGetUserPassword() {
-		UserService userService = new UserServiceImpl(userRepositoy);
-		when(userRepositoy.findPasswordByUserId("hahahohokiss")).thenAnswer(new Answer<String>() {
+		UserService userService = new UserServiceImpl();
+		userService.setUserRepository(userRepository);
+		
+		when(userRepository.findPasswordByUserId("hahahohokiss")).thenAnswer(new Answer<String>() {
 			public String answer(InvocationOnMock invocation) throws Throwable {
 				return "hahahohokiss-password";
 			}
@@ -47,20 +74,12 @@ public class UserServiceTest {
 		
 	}
 	
-	@Test(expected = UserNotFoundException.class)
-	public void testGetUserByUserIdFail() {
-		UserService userService = new UserServiceImpl(userRepositoy);
-		when(userRepositoy.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
-			public User answer(InvocationOnMock invocation) throws Throwable {
-				return null;
-			}
-		});
-		userService.getUserByUserId("hahahohokiss");
-	}
 	
+	// User 추가하기
 	@Test
 	public void testAddUser() {
-		UserService userService = new UserServiceImpl(userRepositoy);
+		UserService userService = new UserServiceImpl();
+		userService.setUserRepository(userRepository);
 		
 		User user = new User();
 		user.setId("hahahohokiss");
@@ -69,13 +88,13 @@ public class UserServiceTest {
 		user.setExplanation("제주대학교");
 		user.setImageUrl("hahahohokiss-image-url");
 		
-		when(userRepositoy.insert(user)).thenAnswer(new Answer<User>() {
+		when(userRepository.insert(user)).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				User user = (User)invocation.getArguments()[0];
 				return user;
 			}
 		});
-		when(userRepositoy.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
+		when(userRepository.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
 			public User answer(InvocationOnMock invocation) throws Throwable {
 				User user = new User();
 				user.setId("hahahohokiss");
@@ -89,5 +108,62 @@ public class UserServiceTest {
 		userService.add(user);
 		user = userService.getUserByUserId("hahahohokiss");
 		assertThat(user.getId(), is("hahahohokiss"));
+	}
+	
+	
+	// Following 하기
+	@Test
+	public void testRegisterFollowing() {
+		UserService userService = new UserServiceImpl();
+		userService.setFollowRepository(followRepository);
+		when(followRepository.addFollowing("followerUserId", "followingUserId")).thenAnswer(new Answer<Follow>() {
+			public Follow answer(InvocationOnMock invocation) throws Throwable {
+				Follow follow = new Follow();
+				follow.setFollowerUserId((String) invocation.getArguments()[0]);
+				follow.setFollowingUserId((String) invocation.getArguments()[1]);
+				return follow;
+			}
+		});
+
+		String followingUserID = userService.registerFollowing("followerUserId", "followingUserId");
+		assertThat(followingUserID, is("followingUserId"));
+	}
+	
+//	@Test
+	public void testGetFollower() {
+		UserService userService = new UserServiceImpl();
+		userService.setFollowRepository(followRepository);
+		List<User> followers =  userService.getFollower("hahahohokiss");
+		//  followRepository 에서 follower_Id를 가져옴
+		//  가져온 아이디를 getUserByUserId(id)로 User를 가져옴
+		assertThat(followers.get(0).getId(), is("follower_1"));
+	}
+	
+	@Test
+	public void testGetFollowersId() {
+		UserService userService = new UserServiceImpl();
+		userService.setFollowRepository(followRepository);
+		
+		when(followRepository.getFollowerId("hahahohokiss")).thenAnswer(new Answer<List<String>>() {
+			public List<String> answer(InvocationOnMock invocation) throws Throwable {
+				List<String> followersId = new ArrayList<String>();
+				followersId.add("follower_1");
+				followersId.add("follower_2");
+				return followersId;
+			}
+		});
+		List<String> followersId = userService.getFollowersId("hahahohokiss");
+		assertThat(followersId.get(0), is("follower_1"));
+		assertThat(followersId.get(1), is("follower_2"));
+	}
+	
+	@Test
+	public void testGetFollowingUser() {
+		
+	}
+	
+	@Test
+	public void testGetAllUser() {
+		
 	}
 }
