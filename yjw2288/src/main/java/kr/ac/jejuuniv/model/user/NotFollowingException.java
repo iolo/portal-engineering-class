a4dd23ac32df53cd11476f19da0685e4e08268d5
yjw2288 <yjw2288@gmail.com>
2012-06-05 16:01:43 +0000

    청소중....

--- yjw2288/src/main/java/kr/ac/jejuuniv/model/user/NotFollowingException.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.jejuuniv.model.user;
-
-public class NotFollowingException extends RuntimeException {
-	private static final long serialVersionUID = 3677085728575901957L;
-
-	public NotFollowingException(String message) {
-		super(message);
-	}
-}
