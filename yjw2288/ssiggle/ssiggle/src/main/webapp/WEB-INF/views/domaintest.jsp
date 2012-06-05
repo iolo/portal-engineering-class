30145974210f4ef4bd31820d3a8cf6eba85fa78b
yjw2288@gmail.com <yjw2288@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-05 15:59:53 +0000

    나의 실수...

--- yjw2288/ssiggle/ssiggle/src/main/webapp/WEB-INF/views/domaintest.jsp
+++
@@ -1,7 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
-
-아이디 : ${user.id}<br/>
-이름 : ${user.name}<br/>
-패스워드 : ${user.password}
--- yjw2288/ssiggle/usermanager/src/main/java/kr/ac/jejuuniv/repository/IdExistNotException.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.jejuuniv.repository;
-
-public class IdExistNotException extends RuntimeException {
-	private static final long serialVersionUID = -2744884938710514939L;
-
-	public IdExistNotException(String string) {
-		super(string);
-	}
-}
