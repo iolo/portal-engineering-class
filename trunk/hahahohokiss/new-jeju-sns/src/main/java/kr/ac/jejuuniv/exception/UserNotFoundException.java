16f8a9c704800b3885779b2928e683fd6873a48f
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-05 16:19:12 +0000

    - 수정 파일 삭제 및 UserServiceTest class 에 testGetUser method 추가 및 성공

--- trunk/hahahohokiss/new-jeju-sns/src/main/java/kr/ac/jejuuniv/exception/UserNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package kr.ac.jejuuniv.exception;
-
-public class UserNotFoundException extends RuntimeException{
-	private static final long serialVersionUID = 3022003324218197230L;
-}
--- trunk/hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
+++ trunk/hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
@@ -1,5 +1,38 @@
 package kr.ac.jejuuniv.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import kr.ac.jejuuniv.model.User;
+import kr.ac.jejuuniv.repository.UserRepository;
+import kr.ac.jejuuniv.service.impl.UserServiceImpl;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
-		
+	
+	@Mock
+	UserRepository userRepository;
+	
+	@Test
+	public void testGetUser() {
+		UserService userService = new UserServiceImpl(userRepository);
+		when(userRepository.findUserByUserId("hahahohokiss")).thenAnswer(new Answer<User>() {
+			public User answer(InvocationOnMock invocation) throws Throwable {
+				User user = new User();
+				user.setLoginId("hahahohokiss");
+				return user;
+			}
+		});
+		User user = userService.getUser("hahahohokiss");
+		assertThat(user.getLoginId(), is("hahahohokiss"));
+	}
 }
