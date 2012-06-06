ee3fdde154158dce1804ed09ee7baf65d0cdf983
hahahohokiss <hahahohokiss@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-06 15:12:37 +0000

    - UserServiceTest Test method 추가 및 성공

--- hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
+++ hahahohokiss/new-jeju-sns/src/test/java/kr/ac/jejuuniv/service/UserServiceTest.java
@@ -22,6 +22,19 @@ import kr.ac.jejuuniv.service.impl.UserServiceImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
+	/* 
+	 * User 가져오기 - 성공
+	 * User 갱신 - 성공
+	 * Password 가져오기 - 성공
+	 * Tweet을 Id로 가져오기 - 성공
+	 * Following User 가져오기 - 성공
+	 * Follower 가져오기 - 성공
+	 * Following 하기 - 성공
+	 * Following User Tweet가져오기 - 성공
+	 * All User 가져오기 - 성공
+	 * Tweet 쓰기 - 
+	 * Tweet 삭제 -
+	*/
 	
 	@Mock
 	UserRepository userRepository;
@@ -38,6 +51,21 @@ public class UserServiceTest {
 		});
 		User user = userService.getUser("hahahohokiss");
 		assertThat(user.getLoginId(), is("hahahohokiss"));
+	}
+	
+	@Test
+	public void testModifyUser() {
+		UserService userService = new UserServiceImpl(userRepository);
+		User user = new User();
+		user.setUsername("modfiyUser");
+		
+		when(userRepository.updateUser(user)).thenAnswer(new Answer<User>() {
+			public User answer(InvocationOnMock invocation) throws Throwable {
+				return (User) invocation.getArguments()[0];
+			}
+		});
+		user = userService.modifyUser(user);
+		assertThat(user.getUsername(), is("modfiyUser"));
 	}
 	
 	@Test
@@ -109,6 +137,46 @@ public class UserServiceTest {
 		List<User> followerUser = userService.getFollowerUser("hahahohokiss");
 		assertTrue(followerUser.size() > 0);
 		assertThat(followerUser.get(0).getLoginId(), is("followerUser"));
+	}
+
+	@Test
+	public void testAddFollow() {
+		UserService userService = new UserServiceImpl(userRepository);
+		
+		User user = new User();
+		User following = new User();
+		
+		userService.addFollow(user.getLoginId(), following.getLoginId());
+	}
+	
+	@Test
+	public void testGetFollowingTweet() {
+		UserService userService = new UserServiceImpl(userRepository);
+		
+		when(userRepository.findFollowingUserByUserId("hahahohokiss")).thenAnswer(new Answer<List<User>>() {
+			public List<User> answer(InvocationOnMock invocation) throws Throwable {
+				List<User> users = new ArrayList<User>();
+				User user = new User();
+				user.setLoginId("followingUser");
+				users.add(user);
+				return users;
+			}
+		});
+		
+		when(userRepository.findFollowingUserTweetByFollowingUser("hahahohokiss")).thenAnswer(new Answer<List<Tweet>>() {
+			public List<Tweet> answer(InvocationOnMock invocation) throws Throwable {
+				List<Tweet> tweets = new ArrayList<Tweet>();
+				Tweet tweet = new Tweet();
+				User user = new User();
+				user.setLoginId("followingUser");
+				tweet.setUser(user);
+				tweets.add(tweet);
+				return tweets;
+			}
+		});
+		List<Tweet> followingUserTweets = userService.getFollowingTweet("hahahohokiss");
+		assertTrue(followingUserTweets.size() > 0);
+		assertThat(followingUserTweets.get(0).getUser().getLoginId(), is("followingUser"));
 	}
 	
 	@Test
