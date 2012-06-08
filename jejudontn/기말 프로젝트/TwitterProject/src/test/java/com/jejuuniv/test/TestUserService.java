1ba9de2f40d624bcd8298b4e6e8bcf5af48ab739
jejudontn <jejudontn@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-08 09:07:34 +0000

    modify 오류 수정// integration Test 시작할것.

--- jejudontn/기말 프로젝트/TwitterProject/src/test/java/com/jejuuniv/test/TestUserService.java
+++ jejudontn/기말 프로젝트/TwitterProject/src/test/java/com/jejuuniv/test/TestUserService.java
@@ -1,9 +1,23 @@
 package com.jejuuniv.test;
 
+import kr.ac.jejuuniv.repository.UserMapper;
+import kr.ac.jejuuniv.service.UserService;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestUserService {
+	
+	UserService userService;
+	@Mock
+	UserMapper userMapper;
+	@Test
+	public void FollowUserTest(){
+		
+		
+	}
 
 }
