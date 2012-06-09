7dea0b0a68fd2e07ad5387bc49c53a8bbd60d045
sdxd0919 <sdxd0919@gmail.com>
2012-06-09 10:52:06 +0000

    tweet 추가하기 수정함!!

--- sdxd0919/twitter/src/main/java/kr/ac/jejuuniv/Controller/WriteController.java
+++ sdxd0919/twitter/src/main/java/kr/ac/jejuuniv/Controller/WriteController.java
@@ -3,7 +3,6 @@ package kr.ac.jejuuniv.Controller;
 import javax.servlet.http.HttpSession;
 
 import kr.ac.jejuuniv.Model.Tweet;
-import kr.ac.jejuuniv.Model.User;
 import kr.ac.jejuuniv.Service.TweetService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +10,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/write")
 public class WriteController {
 
 	@Autowired
 	private TweetService tweetService;
 
-	@RequestMapping
-	public void action(HttpSession session, String message){
-		int userNum = (Integer) session.getAttribute("userNum");
+	@RequestMapping("/write") 
+	public void loginpopup(){ 
+	}
 
-		if(message != null){
-			Tweet tweet = new Tweet(userNum, message);
-			tweetService.addTweet(tweet);
-		}
+
+	@RequestMapping("/addTweet")
+	public String action(HttpSession session, String message, String url) {
+		int userNum = (Integer) session.getAttribute("userNum");
+		
+		url = url.substring(30, url.length());
+		Tweet tweet = new Tweet(userNum, message);
+		tweetService.addTweet(tweet);
+		
+		return "redirect:/" + url;
 	}
 }
