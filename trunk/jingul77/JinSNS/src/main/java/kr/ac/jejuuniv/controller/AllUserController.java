58443cb47b740724806874b080ee1010d96e48a7
jingul77 <jingul77@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-12 16:16:09 +0000

    follwing,follower,alluser 기본 구현

---
+++ trunk/jingul77/JinSNS/src/main/java/kr/ac/jejuuniv/controller/AllUserController.java
@@ -0,0 +1,14 @@
+package kr.ac.jejuuniv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/alluser")
+public class AllUserController {
+	
+	@RequestMapping()
+	public void alluser(){
+		
+	}	
+}
