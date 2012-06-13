914fe3bc373c588a856e144d708e28d623f5be17
seokjun02@gmail.com <seokjun02@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-13 11:40:17 +0000

    userpost관련 업데이트

--- trunk/seokjun02/TwitterSJ/src/main/java/kr/ac/jejuuniv/model/Post.java
+++ trunk/seokjun02/TwitterSJ/src/main/java/kr/ac/jejuuniv/model/Post.java
@@ -4,6 +4,42 @@ import org.apache.ibatis.type.Alias;
 
 @Alias("Post")
 public class Post {
-	private String post;
+	private int sequenceNumber;
+	private String content;
+	private String writeDate;
+	private String userName;
+	private String photo;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public int getSequenceNumber() {
+		return sequenceNumber;
+	}
+	public void setSequenceNumber(int sequenceNumber) {
+		this.sequenceNumber = sequenceNumber;
+	}
+	
 
 }
---
+++ trunk/seokjun02/TwitterSJ/src/main/webapp/WEB-INF/views/personal.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>TwitterSJ</title>
+</head>
+<body>
+	<div id="Header">${userId}님이 로그인 되었습니다.!!!</div>
+	<div id="Menu">
+		<a href="personal">목록보기</a><br /> <a href="/TwitterSJ/logout">logout</a>
+	</div>
+	<div id="Content">
+		<table>
+			<tbody>
+				<c:forEach items="${postList}" var="post">
+					<tr>
+						<td>${post.photo}</td>
+						<td>${post.sequenceNumber}</td>
+						<td>${post.content}</td>
+						<td><a href="remove?id=${post.userId}">삭제</a></td>
+						<td>${post.writeDate}</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
