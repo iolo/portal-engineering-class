76d6120ca717d4dc8fdda9482c51432a0e53ef51
seokjun02@gmail.com <seokjun02@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2012-06-14 09:09:07 +0000

    rename modifyUser to modifyuser

--- trunk/seokjun02/TwitterSJ/src/main/java/kr/ac/jejuuniv/controller/ModifyUserController.java
+++ trunk/seokjun02/TwitterSJ/src/main/java/kr/ac/jejuuniv/controller/ModifyUserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/service/modifyUser")
+@RequestMapping("/service/modifyuser")
 public class ModifyUserController {
 	@Autowired
 	private LoginService loginService;
@@ -24,7 +24,7 @@ public class ModifyUserController {
 		user.setPassword("");
 		
 		model.addAttribute("user", user);
-		return "modifyUser";
+		return "modifyuser";
 	}
 
 }
