ee50f03697e23f7475e4f12ce02ca53a2fa74daf
sdxd0919 <sdxd0919@gmail.com>
2012-06-14 05:30:31 +0000

    프로필 수정! [이미지 경로 변경]

--- sdxd0919/new-twitter/src/main/java/kr/ac/jejuuniv/Controller/UploadFileData.java
+++ sdxd0919/new-twitter/src/main/java/kr/ac/jejuuniv/Controller/UploadFileData.java
@@ -12,21 +12,28 @@ public class UploadFileData {
 	public String uploadFile(MultipartFile file, String userId, String name) throws IOException{
 		byte[] byteFile = file.getBytes();
 		
-		String path = "/Users/HSY/Documents/workspace-sts-2.9.0.RELEASE/workspace/PortalService/twitter/src/main/webapp/resources/profileImg";
+		System.out.println("uploadfile!!!!!");
+		
+		String path = "/Users/HSY/Documents/workspace-sts-2.9.0.RELEASE/workspace/PortalService/new-twitter/src/main/webapp/resources/profileImg";
 		path = path + "/" + userId + "/";
+		
+		System.out.println("path =  " + path);
 		
 		File dirPaht = new File(path);
 		if(!dirPaht.exists()){
+			System.out.println("path nono...");
 			dirPaht.mkdir();
+			System.out.println("dir make!");
 		}
-		
+
 		File uploadedFile = new File(path + name);
 		FileOutputStream fos;
 		fos = new FileOutputStream(uploadedFile);
 		fos.write(byteFile);
 		fos.close();
 
-		String imagePath = "/twitter/resources/profileImg" + userId + "/" + name;
+		String imagePath = "/twitter/resources/profileImg/" + userId + "/" + name;
+		System.out.println("fileupload  + imagepath = " + imagePath);
 		return imagePath;
 	}
 }
