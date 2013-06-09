570e0c5f505c126d0825f67814b9f609c8c44c07
grasia08@gmail.com <grasia08@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2013-06-09 12:04:31 +0000

    ./jejunu/ 이후 설정되지 않는 페이지로 주소가 오는 경우 처리 mybatis dependency 설정

---
+++ trunk/juyoung/Comment/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<html>
+<head>
+	<title>non</title>
+</head>
+<body>
+<h1>
+	없는 페이지입니다.
+</h1>
+<a href="http://localhost:8080/jejunu">go to home</a>
+</body>
+</html>
--- trunk/juyoung/Comment/src/test/java/kr/ac/jejunu/CommentDaoTest.java
+++ trunk/juyoung/Comment/src/test/java/kr/ac/jejunu/CommentDaoTest.java
@@ -1,12 +1,11 @@
 package kr.ac.jejunu;
 
-
 import java.util.List;
 
 import kr.ac.jejunu.model.comment;
 import kr.ac.jejunu.repositry.CommentDao;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +13,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value="classpath:/spring/dao-context.xml")
-public class CommentTest {
-	
-	//@Autowired
-	//private CommentDao dao;
+@ContextConfiguration(value="classpath:spring/dao-context.xml")
+public class CommentDaoTest {
+	@Autowired
+	private CommentDao dao;
 	
 	@Test
-	public void listCall(){
-		//List<comment> list = dao.getList();
-		assertNull(null);
+	public void getlist(){
+		List<comment> list = dao.getList();
+		Assert.assertNull(list);
 	}
+
 }
