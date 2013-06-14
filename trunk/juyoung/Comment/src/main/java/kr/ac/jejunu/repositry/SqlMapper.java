cce4410bd9fddfbc9770029579005caae4d8f50c
grasia08@gmail.com <grasia08@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2013-06-14 09:29:07 +0000

    삭제, 날자 테스

--- trunk/juyoung/Comment/src/main/java/kr/ac/jejunu/repositry/SqlMapper.java
+++ trunk/juyoung/Comment/src/main/java/kr/ac/jejunu/repositry/SqlMapper.java
@@ -2,10 +2,10 @@ package kr.ac.jejunu.repositry;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.junit.runners.Parameterized.Parameter;
 
 import kr.ac.jejunu.model.Comment;
 
@@ -17,5 +17,8 @@ public interface SqlMapper {
 	
 	@Insert(value = { "INSERT INTO comment.list (userUrl, writer, commentText, positive, negative, regTime) VALUES (#{userUrl}, #{writer}, #{commentText}, #{positive}, #{negative}, #{regTime});" })
 	void insertComment(Comment comment);
+
+	@Delete(value = {"DELETE FROM comment.list WHERE listId = #{listId}"})
+	void deleteComment(@Param("listId") int listId);
 
 }
