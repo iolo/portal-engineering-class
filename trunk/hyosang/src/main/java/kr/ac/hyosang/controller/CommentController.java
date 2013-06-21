15ca065724b3ea9e7563a5bfe90bb149f8954060
holyeggs@gmail.com <holyeggs@gmail.com@6a6b15d0-8878-11dd-9899-c7ba65f7c4c7>
2013-06-21 08:44:49 +0000

    댓글 작성 구현 - 유저 데이터와 관련없이 단순 작성

--- trunk/hyosang/src/main/java/kr/ac/hyosang/controller/CommentController.java
+++ trunk/hyosang/src/main/java/kr/ac/hyosang/controller/CommentController.java
@@ -3,12 +3,12 @@ package kr.ac.hyosang.controller;
 import java.util.List;
 
 import kr.ac.hyosang.model.Comment;
-import kr.ac.hyosang.repository.CommentDao;
 import kr.ac.hyosang.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,7 +21,21 @@ public class CommentController {
 		List<Comment> comment = commentService.getCommentList();
 		model.addAttribute("commentList", comment);
 		
-		return "home";
+		return "list";
+	}
+	
+	
+	@RequestMapping(value = "/write")
+	public String write() {
+		return "write";
+	}
+	
+	@RequestMapping(value = "/writeprocess")
+	public String writeProcess(@ModelAttribute("comment") Comment comment) {
+		comment.setComment(comment.getComment());
+		comment.setPassword("임의 페스워드! 유저 모델 만든 다음에 유저 페스워드로 입력되도록 수정합시다.");
+		commentService.write(comment);
+		return "redirect:/";
 	}
 	
 }
