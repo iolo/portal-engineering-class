6a9acf28a23aeaaa54e5634b5faf48a293137b37
grasia08@gmail.com <grasia08@gmail.com>
2013-06-25 01:53:05 +0000

    회원가입 및 회원 가입시 이미지 업로드, url을 이용한 이미지 리턴

--- juyoung/Comment/src/test/java/kr/ac/jejunu/CommentServiceAndTransactionalTest.java
+++ juyoung/Comment/src/test/java/kr/ac/jejunu/CommentServiceAndTransactionalTest.java
@@ -46,74 +46,50 @@ public class CommentServiceAndTransactionalTest {
 		service.writeComment(comment);
 	}
 
-	//
-	// @Test
-	// public void deleteComment() {
-	// mapper.deleteComment(1);
-	// }
-	//
-	// @Test
-	// public void dateTest() {
-	// List<Comment> list = mapper.selectAll();
-	// for (int listCount = 0; listCount < list.size(); listCount++) {
-	// Comment comment = list.get(listCount);
-	// // System.out.println(comment.getListId() + " : "
-	// // + comment.getRegTime());
-	// }
-	// }
-	//
-	// @Test
-	// public void joinUser() {
-	// try {
-	// UserInfo user = new UserInfo();
-	// user.setId("ju");
-	// user.setName("김주영");
-	// user.setPassword("1123");
-	// user.setProflie("안녕하세요 김주영입니다.");
-	// user.setProflie_url("url");
-	// mapper.insertUser(user);
-	// } catch (DuplicateKeyException e) {
-	// System.out.println("가입 되어 있습니다.");
-	// }
-	// }
-	//
-	// @Test
-	// public void selectUserInfo() {
-	// UserInfo user = mapper.selectUser("kim");
-	// assertEquals(user.getName(), "김주영");
-	// assertEquals(user.getPassword(), "1123");
-	// assertEquals(user.getProflie(), "안녕하세요 김주영입니다.");
-	// assertEquals(user.getProflie_url(), "url");
-	// }
-	//
-	// @Test
-	// public void getComment() {
-	// Comment comment = mapper.selectComment(2);
-	// }
-	//
-	// @Test
-	// public void userLikeComment() {
-	// // 1. id와 선택된 코멘드 가져오기
-	// UserInfo user = mapper.selectUser("kim");
-	// Comment comment = mapper.selectComment(5);
-	// // 2. id로 코멘트에 평가를 했는지 확인하기
-	// try {
-	// mapper.selectUserLikeCheck(user.getId(), comment.getListId());
-	// // 3-1. 평가를 하지 않았을 때 평가를 입력
-	// System.out.println("평가 불가");
-	// } catch (Exception e) {
-	// // 3-2. 평가를 한 경우 정지
-	// System.out.println("평가 가능");
-	// // 긍정
-	// mapper.updatePositiveUser(comment.getListId());
-	// // 부정
-	// // mapper.negativeUser(comment.getListId());
-	// mapper.insertUserLikeCommentCheck(user.getId(), comment.getListId());
-	// }
-	// }
-	//
+	@Test
+	public void deleteComment() {
+		service.removeComment(1);
+	}
 
-	//
+	@Test
+	public void joinUser() {
+		try {
+			UserInfo user = new UserInfo();
+			user.setId("ju");
+			user.setName("김주영");
+			user.setPassword("1123");
+			user.setProflie("안녕하세요 김주영입니다.");
+			user.setProflie_url("url");
+			service.joinUser(user);
+		} catch (DuplicateKeyException e) {
+			System.out.println("가입 되어 있습니다.");
+		}
+	}
+
+	@Test
+	public void selectUserInfo() {
+		UserInfo user = service.findUserInfoById("kim");
+		assertEquals(user.getName(), "김주영");
+		assertEquals(user.getPassword(), "1123");
+		assertEquals(user.getProflie(), "안녕하세요 김주영입니다.");
+		assertEquals(user.getProflie_url(), "url");
+	}
+	
+	 @Test
+	 public void getComment() {
+	 Comment comment = service.findCommentByListId(2);
+	 }
+	
+	 @Test
+	 public void userLikeComment() {
+	 // 1. id와 선택된 코멘드 가져오기
+	 String user = service.findUserInfoById("kim").getId();
+	 int comment = service.findCommentByListId(5).getListId();
+	 // 2. id로 코멘트에 평가를 했는지 확인하기
+	 service.addUserLikeComment(user, comment, "ne");
+	 }
+	
+
 	// 페이징 처리에 필요한 정보 추출
 	@Test
 	public void pagingParamsExtr() {
@@ -125,7 +101,7 @@ public class CommentServiceAndTransactionalTest {
 			for (int Count = 0; Count < pageComments.size(); Count++) {
 				System.out.println(pageComments.get(Count).getListId());
 			}
-			System.out.println("page end : "+page+"페이지");
+			System.out.println("page end : " + page + "페이지");
 		}
 	}
 }
