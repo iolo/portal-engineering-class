dcbda7ec06d00541ce61b48e64a1707ec7497330
bbong8926 <bbong8926@gmail.com>
2013-06-25 05:13:56 +0000

    추천/반대 기능 구현

--- BongGyun/src/main/java/kr/bong/replyboard/ReplyController.java
+++ BongGyun/src/main/java/kr/bong/replyboard/ReplyController.java
@@ -2,10 +2,13 @@ package kr.bong.replyboard;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import kr.bong.replyboard.model.Rating;
 import kr.bong.replyboard.model.Reply;
 import kr.bong.replyboard.model.User;
+import kr.bong.replyboard.service.RatingService;
 import kr.bong.replyboard.service.ReplyService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +22,16 @@ public class ReplyController {
 	@Autowired
 	ReplyService replyService;
 	
+	@Autowired
+	RatingService ratingService;
+	
 	@RequestMapping(value = "/")
-	public String list(Model model) {
+	public String list(Model model, HttpServletRequest request) {
 		List<Reply> replyList = replyService.getList();
+		String duple = request.getParameter("duple");
 		
 		model.addAttribute("replyList", replyList);
+		model.addAttribute("duple", duple);
 		
 		return "reply/list";
 	}
@@ -33,7 +41,7 @@ public class ReplyController {
 		User user = (User) session.getAttribute("user");
 		
 		if(user == null) {
-			return "redirect:/login";
+			return "redirect:/login?referURL=/write";
 		}
 		
 		return "reply/write";
@@ -47,5 +55,41 @@ public class ReplyController {
 		replyService.write(reply);
 		
 		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/upRating")
+	public String upRating(HttpSession session, HttpServletRequest request) {
+		User user = (User) session.getAttribute("user");
+		String strReplyNo = request.getParameter("replyNo");
+		int replyNo = Integer.parseInt(strReplyNo);
+		
+		Rating rating = new Rating();
+		rating.setId(user.getId());
+		rating.setReplyNo(replyNo);
+		
+		int result = ratingService.upRating(rating);
+		if(result == -1){
+			return "redirect:/?duple=1";
+		} else {
+			return "redirect:/";
+		}
+	}
+
+	@RequestMapping(value = "/downRating")
+	public String downRating(HttpSession session, HttpServletRequest request) {
+		User user = (User) session.getAttribute("user");
+		String strReplyNo = request.getParameter("replyNo");
+		int replyNo = Integer.parseInt(strReplyNo);
+		
+		Rating rating = new Rating();
+		rating.setId(user.getId());
+		rating.setReplyNo(replyNo);
+		
+		int result = ratingService.downRating(rating);
+		if(result == -1){
+			return "redirect:/?duple=2";
+		} else {
+			return "redirect:/";
+		}
 	}
 }
